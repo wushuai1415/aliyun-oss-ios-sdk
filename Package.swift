1007c49bb122e0b119b6b0e173681ed97071e990
ws <ws1350@hotmail.com>
2023-02-08 15:31:52 +0800

    no message

--- Package.swift
+++ Package.swift
@@ -18,15 +18,16 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .systemLibrary(name: "libresolv.tbd"),
-        .systemLibrary(name: "CoreTelephony.framework"),
-        .systemLibrary(name: "SystemConfiguration.framework"),
         .target(
             name: "AliyunOSSiOS",
-            dependencies: [.byName(name: "libresolv.tbd")],
+            dependencies: [],
             path: "AliyunOSSSDK",
-            sources: ["OSSTask"],
-            publicHeadersPath: "include")
+            linkerSettings: [
+                .linkedLibrary("libresolv"),
+                .linkedFramework("CoreTelephony"),
+                .linkedFramework("SystemConfiguration")
+            ]
+        )
 //        .testTarget(
 //            name: "AliyunOSSiOSTests",
 //            dependencies: ["AliyunOSSiOS"],
